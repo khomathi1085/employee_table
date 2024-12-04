d8b46cdc5269f8948ad48f982f0d7525db1be012
Khomathi <khomathi1085@gmail.com>
2024-12-04 19:01:34 +0530

    Updated MySQL connection config and added environment variables

--- frontend/src/pages/AddEmployee.jsx
+++ frontend/src/pages/AddEmployee.jsx
@@ -5,7 +5,7 @@ import EmployeeForm from "../components/EmployeeForm";
 const AddEmployee = () => {
   const handleSubmit = async (values) => {
     try {
-      const response = await axios.post("http://localhost:5000/add-employee", values);
+      const response = await axios.post(`${process.env.REACT_APP_BACKEND_URL}/add-employee`, values);
       alert(response.data);
     } catch (error) {
       alert(error.response.data || "An error occurred");
